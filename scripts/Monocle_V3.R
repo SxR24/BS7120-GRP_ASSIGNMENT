3ff18ca6f3b0872d566c34a5c781b5926c01942b
JHazzrd <jed_hazzard@outlook.com>
2026-03-31 14:51:29 +0100

    Monocle script [last] attempt

--- scripts/Monocle_V3.R
+++ scripts/Monocle_V3.R
@@ -18,7 +18,7 @@ BiocManager::install(version = "3.21") # Previous version 1.30.27
 # Rmpfr fails; Header file mpfr.h not found... 
 # Configuration failed for 'units'; libudunits2.so not found
 # Configuration failed for 'gdtools; cairo-ft.h not found
-# Configuration failed for 'sf'; gdal-config not found or not exectuable
+# Configuration failed for 'sf'; gdal-config not found or not executable
 # Configuration failed for 'exactextractr'; geos-config not found or not executable
 # Loading failed for 'stars'; found sf but wrong version (>= 1.0.19 required)
 # Potential Other Errors - TL;DR lots of errors but so far no problems
@@ -56,34 +56,53 @@ library(SeuratWrappers)
 library(Matrix)
 library(patchwork)
 library(ggplot2)
-
+library(gdata)
 # Monocle pipeline
 
-# Import cell metadata and expression matrix; no gene metadata as not required
+# Import cell metadata and expression matrix; gene metadata only list of gene names.
 
 cell_metadata<- read_delim("cell_metadata_1.tsv", 
                             delim = "\t", escape_double = FALSE, 
                             trim_ws = TRUE)
-expression_matrix <- read_delim("expression_matrix2.tsv", 
+expression_matrix <- read_delim("2026_UoL_prj_gene_matrix_f.tsv", 
                                 delim = "\t", escape_double = FALSE, 
                                 trim_ws = TRUE)
 gene_metadata <- read_delim("gene_metadata.tsv", 
                                  delim = "\t", escape_double = FALSE, 
                                  trim_ws = TRUE)
 
-# Adjust dfs and convert expression_matrix into a matrix format
+SraRunTable_1_ <- read_csv("SraRunTable (1).csv") # More Cell Metadata
+
+# Adjust dfs 
+
+expression_matrix<- expression_matrix[ !(expression_matrix$gene == "species"), ]
 
 expression_matrix <- expression_matrix %>% tibble::column_to_rownames(., var = names(.)[1])
 cell_metadata <- cell_metadata %>% tibble::column_to_rownames(., var = names(.)[1])
 gene_metadata <- gene_metadata %>% tibble::column_to_rownames(., var = names(.)[1])
-mat <- as.matrix(expression_matrix)
+
+# Select Fetal NC Cells
+
+f_nc_cells <- subset(SraRunTable_1_, tissue == "Fetal neocortex")
+ex_mat_fnc <- select(expression_matrix, contains(f_nc_cells$Run))
+inter_neur <- c("SRR2967628","SRR2967645","SRR2967696","SRR2967704","SRR2967725")
+ex_mat_fnc <- select(ex_mat_fnc, -contains(inter_neur))
+ex_mat_fnc <- select(ex_mat_fnc, -contains("SRR2967658")) # Mystery Missing Cell?
+
+# Select organoids (fig4) NEEDS ADJUSTING, INFO FROM FIG 3 and 5SA; 157 cells in TOTAL
+
+org_cells <- subset(SraRunTable_1_, tissue == "Microdissected cortical-like ventricle from cerebral organoid")
+ex_mat_org <- select(expression_matrix, contains(org_cells$Run))
+
+# Convert to Matrix
+
+mat_f <- as.matrix(ex_mat_fnc) 
+
+mat_o <- as.matrix(ex_mat_org)
 
 
-# Monocles main class; cell_data_set
 
-cds <- new_cell_data_set(mat,
-                         cell_metadata = cell_metadata,
-                         gene_metadata = gene_metadata) 
+
 
 # Cells are to be arranged based off of genes used to classify cells from Fig1C; here are the cells
 
@@ -93,24 +112,54 @@ genes_from_fig1c <- c("PAX6", "GLI3", "VIM","SOX2","MYT1L","TBR1","NEUROD6","DCX
                       "HES1","HES6","ASPM","BCL11B"
                  ) 
 
-# Genes selected as of Fig2B
+# Genes selected as of Fig2B; marker genes for verification
 
 fig2b <- c("SOX2","EOMES","MYT1L")
 
+# Fig 2
+# Monocles main class; cell_data_set
+cds <- new_cell_data_set(mat_f,
+                         cell_metadata = cell_metadata,
+                         gene_metadata = gene_metadata) 
+
 # Step 1: Normalize and pre-process the data
-cds <- preprocess_cds(cds, use_genes = genes_from_fig1c)
-# Step 2: Remove batch effects with cell alignment
-cds <- align_cds(cds)
-# Step 3: Reduce the dimensions using UMAP
-cds <- reduce_dimension(cds)
+cds <- preprocess_cds(cds, use_genes = genes_from_fig1c, norm_method = "none",
+                      num_dim = length(genes_from_fig1c)-15)
+
+# Step 3: Reduce the dimensions 
+cds <- reduce_dimension(cds, preprocess_method = "PCA", reduction_method = "PCA",
+                        max_components = 2)
+
+cds@int_colData@listData$reducedDims@listData$UMAP <- cds@int_colData@listData$reducedDims$PCA
 # Step 4: Cluster the cells
-cds <- cluster_cells(cds)
+cds <- cluster_cells(cds, reduction_method = "PCA", cluster_method = "louvain")
+cds@clusters@listData$UMAP <- cds@clusters@listData$PCA
+cds@int_metadata$reduce_dim_metadata$UMAP <- cds@int_metadata$reduce_dim_metadata$PCA
 # Step 5: Learn a graph
+
 cds <- learn_graph(cds)
 # Step 6: Order cells (Starts at AP)
 cds <- order_cells(cds)
 
 
+## TESTING
+cds <- preprocess_cds(cds, use_genes = genes_from_fig1c, norm_method = "none", method = "PCA")
+cds <- align_cds(cds, preprocess_method = "PCA")
+plot_pc_variance_explained(cds)
+cds <- reduce_dimension(cds,reduction_method = "PCA")
+plot_cells(cds, color_cells_by = "CellType", reduction_method = "PCA", cell_size = 2,
+           x = 2, y = 1)
+cds <- cluster_cells(cds, reduction_method = "PCA")
+plot_cells(cds, reduction_method = "PCA", cell_size = 2, color_cells_by = "CellType",
+           x = 2, y = 1)
+
+cds@int_colData@listData$reducedDims@listData$UMAP <- cds@int_colData@listData$reducedDims$PCA
+
+cds <- learn_graph(cds)
+
+cds <- order_cells(cds)
+
+##
 a <- plot_cells(cds, color_cells_by = "CellType",
            label_cell_groups=FALSE,
            label_leaves=TRUE,
@@ -142,10 +191,68 @@ e <- plot_cells(cds, genes = "MYT1L",
                 label_branch_points=TRUE,
                 graph_label_size=1.5,
                 cell_size = 2) + theme(legend.position = "none")
+a
+
 a / (b+c+d+e)
 
 save_monocle_objects(cds=cds, directory_path='monocle_3_cds', comment='Version1 - 18/03')
+# cds <- load_monocle_objects(directory_path='monocle_3_cds')
 
+# Fig 4
+
+cds_4 <- new_cell_data_set(mat_o,
+                         cell_metadata = NULL,
+                         gene_metadata = gene_metadata) 
+
+cds_4 <- preprocess_cds(cds_4, use_genes = genes_from_fig1c,)
+# Step 2: Remove batch effects with cell alignment
+cds_4 <- align_cds(cds_4)
+# Step 3: Reduce the dimensions using UMAP
+cds_4 <- reduce_dimension(cds_4)
+# Step 4: Cluster the cells
+cds_4 <- cluster_cells(cds_4)
+# Step 5: Learn a graph
+cds_4 <- learn_graph(cds_4)
+# Step 6: Order cells (Starts at AP)
+cds_4 <- order_cells(cds_4)
+
+
+a <- plot_cells(cds_4, color_cells_by = "CellType",
+                label_cell_groups=FALSE,
+                label_leaves=TRUE,
+                label_branch_points=TRUE,
+                graph_label_size=1.5,
+                cell_size = 2)
+
+b <- plot_cells(cds_4, color_cells_by = "Zone",
+                label_cell_groups=FALSE,
+                label_leaves=TRUE,
+                label_branch_points=TRUE,
+                graph_label_size=1.5,
+                cell_size = 2)
+c <- plot_cells(cds_4, genes = "SOX2",
+                label_cell_groups=FALSE,
+                label_leaves=TRUE,
+                label_branch_points=TRUE,
+                graph_label_size=1.5,
+                cell_size = 2) 
+d <- plot_cells(cds_4, genes = "EOMES",
+                label_cell_groups=FALSE,
+                label_leaves=TRUE,
+                label_branch_points=TRUE,
+                graph_label_size=1.5,
+                cell_size = 2) + theme(legend.position = "none")
+e <- plot_cells(cds_4, genes = "MYT1L",
+                label_cell_groups=FALSE,
+                label_leaves=TRUE,
+                label_branch_points=TRUE,
+                graph_label_size=1.5,
+                cell_size = 2) + theme(legend.position = "none")
+# a / (b+c+d+e)
+
+a / (c + d + e)
+
+get_citations(cds)
 ## Monocle Objects - Please read for LAMP server!
 
 # Best way to load up the 'monocle_3_cds' is via:
