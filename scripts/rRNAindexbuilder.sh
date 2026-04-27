a1fa84fa75bc4136315818050d1192e72b4f94ad
Randolando <117410002+randolando1@users.noreply.github.com>
2026-04-27 06:23:33 +0100

    Update rRNAindexbuilder.sh
    Improved rRNA comments

--- scripts/rRNAindexbuilder.sh
+++ scripts/rRNAindexbuilder.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-# rRNA index creation for bowtie2
+# #############################################################################
+# rRNA Index Creation for Bowtie2
+# #############################################################################
+# Purpose: Generate a Bowtie2 index from a FASTA file of human rRNA sequences.
+# Input:   4V6X_human_rRNAs.fa sourced from the ARF R package)
+# Output:  A set of index files with the prefix 'rRNA_index'
+# #############################################################################
+
+# Build the Bowtie2 index
 bowtie2-build /home/i/ia256/Downloads/4V6X_human_rRNAs.fa rRNA_index
